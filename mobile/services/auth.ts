4703eeb958598f1381eebff7fadf5d26acb22e7e
Jeremy Vega <jeredev20@gmail.com>
2024-11-13 01:22:32 -0300

    Android & IOS fetchs

--- mobile/services/auth.ts
+++ mobile/services/auth.ts
@@ -1,27 +1,24 @@
 import axios from 'axios'
+import api from '../httpService'
 import { DraftUser, User } from '../types'
 
 export const register = async (user: DraftUser) => {
     try {
-        const url = `${process.env.API_BASE_URL}/auth/register` 
-        const response = await axios.post(url, user)
-
-        console.log(response)
+        const response = await api.post('/auth/register', user);
+        console.log(response);
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
 export const login = async (email: User['email'], password: User['password']) => {
     try {
-        const url = `${process.env.API_BASE_URL}/auth/login`
-        const response = await axios.post(url, {email, password}, {
+        const response = await api.post('/auth/login', { email, password }, {
             withCredentials: true
         })
         
-        return response
-
+        return response;
     } catch (error) {
-        
+        console.error(error);
     }
 }
--- mobile/services/posts.ts
+++ mobile/services/posts.ts
@@ -1,48 +1,46 @@
 import axios, { Axios, AxiosError } from 'axios';
+import api from '../httpService';
 import type { DraftPost, Post } from '../types';
 
 export const createPost = async (postData: DraftPost) => {
-    try {
-        const url = `${process.env.API_BASE_URL}/post/create`
-        console.log(postData)
-        const response = await axios.post(url, postData , {
-            withCredentials: true
-        });
-        console.log(response)
-        return response.data;
-    } catch (error) {
-        console.error('Error creating post:', error)
-        throw error;
-    }
+  try {
+      console.log(postData);
+      const response = await api.post('/post/create', postData, {
+          withCredentials: true
+      });
+      console.log(response);
+      return response.data
+  } catch (error) {
+      console.error('Error creating post:', error)
+      throw error
+  }
 }
 
 export const getPosts = async () => {
-    try {
-        const url = `${process.env.API_BASE_URL}/post/get`
-        const response = await axios(url)
-        console.log(response.data)
-        return response.data
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
+  try {
+      const response = await api.get('/post/get')
+      console.log(response.data)
+      return response.data
+  } catch (error) {
+      console.error(error)
+      throw error
+  }
 }
 
 export const postulation = async (id: Post['post_id']) => {
-    try {
-      const url = `${process.env.API_BASE_URL}/post/postulation/${id}`
-      const response = await axios.post(url, {}, {
-        withCredentials: true,
-        validateStatus: (status) => {
-            return status < 500
+  try {
+      const response = await api.post(`/post/postulation/${id}`, {}, {
+          withCredentials: true,
+          validateStatus: (status) => {
+              return status < 500
           }
-      })
-  
+      });
+
       return {
-        status: response.status,
-        data: response.data
-      }
-    } catch (error) {
+          status: response.status,
+          data: response.data
+      };
+  } catch (error) {
       throw error
-    }
   }
+}
